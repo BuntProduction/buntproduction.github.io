fcbd41565912594e4a4b59fc2ddbc659cb7bad00
BuntProd <buntlicorn@gmail.com>
2022-06-02 19:03:28 +0200

    scriptrecherche

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function myFunction(){
     var search = document.getElementById("search").value;
-    if (search == "Formation" || search == "formation"){
-        window.location = "file:///C:/Users/gatie/Documents/GitHub/Portfolio/" + search;
+    if (search == "Formation" || search == "formation" || search == "profil" || search == "Profil"){
+        window.location = "./pageprofil.html";
     }else if (search == "Projet" || search == "projet" || search == "portfolio" || search == "Portfolio"){
         window.location = "file:///C:/Users/gatie/Documents/GitHub/Portfolio/" + search;
     }else{
